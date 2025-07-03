4b1c0ed06a3e5425ebaafe82ba6eb4f661649604
betimkrasniqi-dpn <betim.krasniqi@dataprognet-ks.com>
2025-07-03 13:43:24 +0200

    ews

--- EWSApi/obj/Debug/net7.0/EWSApi.AssemblyInfo.cs
+++ EWSApi/obj/Debug/net7.0/EWSApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EWSApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0280f38e41e582d406de065d96d8e865d25f9777")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15f37c26f1c78a020b7df7e68dcbe48cccc8592")]
 [assembly: System.Reflection.AssemblyProductAttribute("EWSApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EWSApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
